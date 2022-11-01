d11922055fbddec2b782b5c5f7d21f2e6c152e7a
Haobo Zhang <haobozhang9081@gmail.com>
2022-11-01 08:39:19 -0700

    remove head put in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import { Roboto } from '@next/font/google';
 import type { ReactNode } from 'react';
+import type { PackageJson } from 'type-fest';
+import _packageJson from '../package.json';
 import Providers from '../src/layout/providers';
 import '../src/layout/style.scss';
+
+const packageJson = _packageJson as PackageJson;
 
 const roboto = Roboto( {
 	weight: [ '300', '400', '500', '700' ]
@@ -10,6 +14,23 @@ const roboto = Roboto( {
 export default function RootLayout( { children }: { children: ReactNode } ) {
 	return (
 		<html lang='en' className={roboto.className}>
+			<head>
+				<title>Template</title>
+				<meta charSet='utf-8'/>
+				<meta name='viewport' content='width=device-width, initial-scale=1, viewport-fit=cover'/>
+				<meta key='theme' name='theme-color' content='#039be5'/>
+				{/*seo*/}
+				{'description' in packageJson && <meta name='description' content={packageJson.description}/>}
+				{'keywords' in packageJson && <meta name='keywords' content={packageJson.keywords.join( ', ' )}/>}
+				{'author' in packageJson && <meta name='author' content={packageJson.author as string}/>}
+				{/*pwa*/}
+				<meta name='mobile-web-app-capable' content='yes'/>
+				<link rel='manifest' href='/app.webmanifest'/>
+				<link rel='shortcut icon' href='/favicon.ico'/>
+				{/*ios*/}
+				<meta name='apple-mobile-web-app-capable' content='yes'/>
+				<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'/>
+			</head>
 			<body>
 				<Providers>
 					{children}
