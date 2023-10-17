213175bc1597486a8b650f443d393afb7fee428c
Haobo Zhang <haobozhang9081@gmail.com>
2023-10-16 21:09:05 -0700

    fix

--- src/store/persist.ts
+++ src/store/persist.ts
@@ -2,15 +2,15 @@ const KEY = 'persist';
 
 export function loadState() {
 	try {
-		const serializedState = localStorage.getItem( KEY );
-		if ( !serializedState ) return undefined;
-		return JSON.parse( serializedState );
-	} catch ( e ) {
+		const serializedState = localStorage.getItem(KEY);
+		if (!serializedState) return undefined;
+		return JSON.parse(serializedState);
+	} catch (e) {
 		return undefined;
 	}
 }
 
-export async function saveState( state: any ) {
-	const serializedState = JSON.stringify( state );
-	localStorage.setItem( KEY, serializedState );
+export async function saveState(state: any) {
+	const serializedState = JSON.stringify(state);
+	localStorage.setItem(KEY, serializedState);
 }
