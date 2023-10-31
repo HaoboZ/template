8d830385de9648858b969ffdc675a40e6cfe5517
Haobo Zhang <haobozhang9081@gmail.com>
2023-10-31 16:53:58 -0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import Providers from '@/src/providers';
 import { Analytics } from '@vercel/analytics/react';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import type { ReactNode } from 'react';
 import type { PackageJson } from 'type-fest';
 import _packageJson from '../package.json';
@@ -9,10 +9,13 @@ const packageJson = _packageJson as PackageJson;
 
 export const metadata: Metadata = {
 	title: 'Template',
-	themeColor: '#ffffff',
 	description: packageJson.description,
 	keywords: packageJson.keywords?.join(', '),
 	authors: packageJson.author as any,
+};
+
+export const viewport: Viewport = {
+	themeColor: '#ffffff',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
